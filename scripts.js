414c7433e6416db7d52ea0e8cd60221bb21e8760
ikarotaura <ikarooohh@gmail.com>
2025-04-22 14:03:26 -0300

    sorteador de numero

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+
+
+function generatorNumber() {
+    const entre = Math.ceil(document.querySelector(".input-entre").value)
+    const e = Math.floor(document.querySelector(".input-e").value)
+    const result =  Math.floor(Math.random() * (e - entre + 1) + entre);
+   
+    alert (result)
+
+
+
+}
