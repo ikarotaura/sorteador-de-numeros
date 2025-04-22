f1ae83d6959f50e257a9f74f8a938505a041bfda
ikarotaura <ikarooohh@gmail.com>
2025-04-22 16:08:21 -0300

    sorteador de numeros

--- scripts.js
+++ scripts.js
@@ -5,9 +5,22 @@
 function generatorNumber() {
     const entre = Math.ceil(document.querySelector(".input-entre").value)
     const e = Math.floor(document.querySelector(".input-e").value)
-    const result =  Math.floor(Math.random() * (e - entre + 1) + entre);
+    
+    if( entre >= e ){
+        alert("o valor minimo tem que ser Menor que o valor maximo")
+    }
+    else{
+        const result =  Math.floor(Math.random() * (e - entre + 1) + entre);
+      
+        alert (result)
+    }
+
+    
+    
+    
    
-    alert (result)
+   
+  
 
 
 
